a56fc92709d896fd2db3ceaefd3c4cd737b5b542
Pedro Antonio Moreno <pedro.antonio.moreno@avoristravel.com>
2023-10-17 00:40:47 +0200

    primer commit

---
+++ schemes/movies.js
@@ -0,0 +1,34 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    required_error: 'Title is required'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().positive(),
+  poster: z.string().url({
+    messsage: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Drama', 'Comedy', 'Action', 'Romance', 'Horror', 'Sci-Fi', 'Thriller', 'Adventure', 'Crime']),
+    {
+      required_error: 'Genre is required',
+      invalid_type_error: 'Genre must be an array'
+    }
+  ),
+  rate: z.number().min(0).max(10).default(5)
+})
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie(object) {
+  return movieSchema.partial().safeParse(object)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
